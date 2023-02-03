12327ef8acd225e1748063f9a9307c0ef9be082e
abhijeetatmindstix <113978688+abhijeetatmindstix@users.noreply.github.com>
2023-02-03 12:01:05 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, CardBody, Container } from "reactstrap";
+
+
+const Home = () => {
+    return <div >
+      <Card>
+        <CardBody className="text-center my-1 bg-warning">
+        
+              <h1 className="text-center my-1">Banking Application</h1>
+              <p className="text-center my-1">This is Banking Application. Here we are to help citizen of India.</p>  
+         
+        </CardBody>
+      </Card>
+    </div>
+};
+export default Home;
